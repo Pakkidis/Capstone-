0e774fada633aadfe404a9d0673061c748e5ec65
Pakkidis <44510515+Pakkidis@users.noreply.github.com>
2018-10-26 16:31:18 -0400

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# ## This notebook will be mainly used for the capstone project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[8]:
+
+
+print ('Hello Capstone Project Course!')
+
